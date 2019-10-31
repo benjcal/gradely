f866aa0127725a531f7d46061ec2a1eb5fb02c92
Benjamin Calderon <benj.calderon@gmail.com>
2019-10-31 17:22:39 -0400

    changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,9 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import css from "../css/app.css"
+import "../css/app.css"
 
+import "tachyons"
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
 // in "webpack.config.js".
