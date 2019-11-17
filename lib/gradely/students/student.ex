0e1a1fe78d4b59c8c1c98ef7961020ab48b2fec3
Benjamin Calderon <benj.calderon@gmail.com>
2019-11-17 10:02:06 -0500

    allow updating course

--- lib/gradely/students/student.ex
+++ lib/gradely/students/student.ex
@@ -8,7 +8,7 @@ defmodule Gradely.Students.Student do
     field :first_name, :string
     field :last_name, :string
     many_to_many :courses, Gradely.Courses.Course,
-      join_through: Gradely.Enrollments.Enrollment
+      join_through: Gradely.Enrollments.Enrollment, on_replace: :delete
 
     timestamps()
   end
