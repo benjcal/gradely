5f615c9d178a7289928b55636567c351a021de87
Benjamin Calderon <benj.calderon@gmail.com>
2019-11-18 09:12:43 -0500

    more refractor into components

--- lib/gradely_web/views/component_view.ex
+++ lib/gradely_web/views/component_view.ex
@@ -1,3 +1,7 @@
 defmodule GradelyWeb.ComponentView do
   use GradelyWeb, :view
+
+  def button_classes do
+    "h-8 w-24 flex items-center justify-center shadow rounded border border-transparent hover:border-gray-400 text-gray-400 font-bold hover:text-gray-700"
+  end
 end
