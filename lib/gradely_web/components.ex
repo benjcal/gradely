70a1bd58890f1d04ff31948f149fde1e924e85fd
Benjamin Calderon <benj.calderon@gmail.com>
2019-11-18 08:53:50 -0500

    refractoring

--- lib/gradely_web/components.ex
+++ lib/gradely_web/components.ex
@@ -1,11 +1,13 @@
 defmodule GradelyWeb.Components do
 
   def c(template, assigns) do
+    template = Atom.to_string(template) <> ".html"
     GradelyWeb.ComponentView.render(template, assigns)
   end
 
   def c(template, assigns, do: block) do
-    GradelyWeb.ComponentView.render(template, Keyword.merge(assigns, [do: block]))
+    template = Atom.to_string(template) <> ".html"
+    GradelyWeb.ComponentView.render(template, Map.merge(assigns, %{content: block}))
   end
 end
 
--- lib/gradely_web/views/student_view.ex
+++ lib/gradely_web/views/student_view.ex
@@ -4,4 +4,10 @@ defmodule GradelyWeb.StudentView do
   def is_enrolled(course, enrolled_courses) do
     Enum.find_value(enrolled_courses, fn c -> c.id == course.id end)
   end
+
+  def color(x) do
+    AlchemicAvatar.Color.iwanthue(x)
+    |> Enum.map(fn c -> Integer.to_string(c, 16) end )
+    |> Enum.join("")
+  end
 end
